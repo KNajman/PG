29702313b84d7170e749d59d73bac3763357b858
Karel_Najman <karel.najman@tul.cz>
2022-11-24 12:14:01 +0100

    skoro hotova zeměkoule

--- cviceni06/script.js
+++ cviceni06/script.js
@@ -28,39 +28,65 @@ window.onload = function() {
     gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
     // We need many vertices, because each vertex need
     // own value of normal and UV
-    var vertices = [
-        // Bottom face
-        -1.0, -1.0, -1.0, // 0
-        1.0, -1.0, -1.0, // 1
-        1.0, 1.0, -1.0, // 2
-        -1.0, 1.0, -1.0, // 3
-        // Left face
-        -1.0, -1.0, -1.0, // 4
-        1.0, -1.0, -1.0, // 5
-        1.0, -1.0, 1.0, // 6
-        -1.0, -1.0, 1.0, // 7
-        // Front face
-        1.0, -1.0, -1.0, // 8
-        1.0, 1.0, -1.0, // 9
-        1.0, 1.0, 1.0, // 10
-        1.0, -1.0, 1.0, // 11
-        // Right face
-        1.0, 1.0, -1.0, // 12
-        -1.0, 1.0, -1.0, // 13
-        -1.0, 1.0, 1.0, // 14
-        1.0, 1.0, 1.0, // 15
-        // Back face
-        -1.0, -1.0, -1.0, // 16
-        -1.0, -1.0, 1.0, // 17
-        -1.0, 1.0, 1.0, // 18
-        -1.0, 1.0, -1.0, // 19
-        // Top face
-        -1.0, -1.0, 1.0, // 20
-        1.0, -1.0, 1.0, // 21
-        1.0, 1.0, 1.0, // 22
-        -1.0, 1.0, 1.0 // 23
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    //parametry "kulatosti"
+    var n = 100;
+    var m = 100;
+    //velikost planety
+    var radius = 2.5;
+
+    var vertex = [];
+    var data = [];
+    var textCoord = [];
+
+    for (var i = 0; i <= n; i++) {
+        var theta = i * Math.PI / n;
+        var sinTheta = Math.sin(theta);
+        var cosTheta = Math.cos(theta);
+
+        for (var k = 0; k <= m; k++) {
+
+            var phi = k * 2 * Math.PI / m;
+            var sinPhi = Math.sin(phi);
+            var cosPhi = Math.cos(phi);
+
+            var x = cosPhi * sinTheta;
+            var y = cosTheta;
+            var z = sinPhi * sinTheta;
+            var u = 1 - (k / m);
+            var v = 1 - (i / n);
+
+            data.push(x);
+            data.push(y);
+            data.push(z);
+
+            textCoord.push(u);
+            textCoord.push(v);
+
+            vertex.push(radius * x);
+            vertex.push(radius * y);
+            vertex.push(radius * z);
+        }
+    }
+
+    var indexData = [];
+    for (var i = 0; i < n; i++) {
+        for (var k = 0; k < m; k++) {
+
+            var val1 = i * (m + 1) + k;
+            var val2 = val1 + m + 1;
+
+            indexData.push(val1);
+            indexData.push(val2);
+            indexData.push(val1 + 1);
+
+            indexData.push(val2);
+            indexData.push(val2 + 1);
+            indexData.push(val1 + 1);
+        }
+    }
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertex), gl.STATIC_DRAW);
     gl.vertexAttribPointer(posLoc, 3, gl.FLOAT, false, 0, 0);
 
     // Create buffer for UV coordinates
@@ -68,39 +94,7 @@ window.onload = function() {
     gl.enableVertexAttribArray(uvLoc);
     var uvBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
-    uvs = [
-        // Bottom face
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // Left face
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // Front
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // Right face
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // Back face
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // Top face
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvs), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textCoord), gl.STATIC_DRAW);
     gl.vertexAttribPointer(uvLoc, 2, gl.FLOAT, false, 0, 0);
 
     // Create buffer for vertex normals
@@ -108,60 +102,20 @@ window.onload = function() {
     gl.enableVertexAttribArray(normalLoc);
     var normalBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    normals = [
-        // Bottom face
-        0.0, 0.0, -1.0,
-        0.0, 0.0, -1.0,
-        0.0, 0.0, -1.0,
-        0.0, 0.0, -1.0,
-        // Left face
-        0.0, -1.0, 0.0,
-        0.0, -1.0, 0.0,
-        0.0, -1.0, 0.0,
-        0.0, -1.0, 0.0,
-        // Front
-        1.0, 0.0, 0.0,
-        1.0, 0.0, 0.0,
-        1.0, 0.0, 0.0,
-        1.0, 0.0, 0.0,
-        // Right face
-        0.0, 1.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 1.0, 0.0,
-        // Back face
-        -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0,
-        // Top face
-        0.0, 0.0, 1.0,
-        0.0, 0.0, 1.0,
-        0.0, 0.0, 1.0,
-        0.0, 0.0, 1.0
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
     gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, true, 0, 0);
 
     // Create index buffer
     var indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    var indices = [
-        0, 1, 2, // 0
-        0, 2, 3,
-        4, 5, 6, // 1
-        4, 6, 7,
-        8, 9, 10, // 2
-        8, 10, 11,
-        12, 13, 14, // 3
-        12, 14, 15,
-        16, 17, 18, // 4
-        16, 18, 19,
-        20, 21, 22, // 5
-        20, 22, 23
-    ];
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+    this.console.log(indexData);
+    //had to change the data type
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexData), gl.STATIC_DRAW);
 
     // Create and load image used as texture
     var image = new Image();
-    image.src = "./wood_texture_simple.png";
+    image.src = "./globe_texture.jpg";
     image.onload = function() {
         var texture = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, texture);
@@ -219,7 +173,7 @@ window.onload = function() {
         gl.uniformMatrix3fv(normalLocation, false, normalMatrix);
 
         gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT);
-        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
+        gl.drawElements(gl.TRIANGLES, indexData.length, gl.UNSIGNED_SHORT, 0);
         requestAnimationFrame(render);
     }
 
